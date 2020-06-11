7fabd696050acf73577c046cd82c2d4436aa70ea
selvi <3003mathuselvi>
2020-06-11 10:21:48 -0700

    Sample Maven Project

---
+++ src/test/java/sample/SeleTest.java
@@ -0,0 +1,14 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class SeleTest {
+@Test
+public static void selvi() {
+	System.out.println("Hello Selvi Welcome!!");
+}
+@Test
+public static void samp() {
+	System.out.println("Hello Samp Welcome!");
+}
+}
---
+++ src/test/java/sample/SeleniumTest.java
@@ -0,0 +1,20 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+@Test
+public static void hai() {
+	System.out.println("Hello From Hai");
+}
+
+@Test
+public static void hello() {
+	System.out.println("Hello From hello");
+}
+
+@Test
+public static void test() {
+	System.out.println("Hello From Test");
+}
+}
